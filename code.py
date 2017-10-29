bfadcd6c0735b950444486af1547fce5dd85da3d
Sharath Ram Srinivasan <sharathr2912@gmail.com>
2017-10-28 19:03:05 -0700

    Added sample python code

---
+++ code.py
@@ -0,0 +1,9 @@
+dictionary = {'name': 'Ram', 'age': 26, 
+              'hobbies': ['soccer', 'fifa', 'netflix'],
+              'time_I_wake_up': {'Monday':8, 
+              'Tuesday':10, 
+              'Wednesday': 7}}
+
+print(dictionary['name'])
+print(len(dictionary['hobbies']))
+print(str(dictionary['time_I_wake_up']['Monday']))
